911995fef8b6cb2fef2c0eb077639b53d49d068f
asif-bhuti <asif.vudi@northsouth.edu>
2023-03-14 02:31:35 +0600

    cheking

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,7 +3,8 @@ import Link from "next/link";
 const Home = () => (
   <div>
     <h1>Welcome to the hotel admin app!</h1>
-    <Link href="/signUp">Sign up</Link>
+    <Link href="/signUp">Sign Up</Link>
+    <Link href="/signIn">Sign In</Link>
   </div>
 );
 
