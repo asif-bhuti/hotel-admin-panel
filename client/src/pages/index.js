b7355651a685b19e0a31d09890d7064c2e8224a2
asif-bhuti <asif.vudi@northsouth.edu>
2023-03-19 18:28:28 +0600

    theme setup

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,11 +1,53 @@
-import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
 
-const Home = () => (
-  <div>
-    <h1>Welcome to the hotel admin app!</h1>
-    <Link href="/signUp">Sign Up</Link>
-    <Link href="/signIn">Sign In</Link>
-  </div>
-);
+export default function Home() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
 
-export default Home;
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  return (
+    <>
+      <div className="grid place-items-center h-screen">
+        <div className="flex justify-center">
+          {" "}
+          <h1 className="text-7xl font-bold text-center">
+            {" "}
+            {currentTheme === "dark" ? "Dark" : "Light"}{" "}
+            <span className="text-font-primary dark:text-font-secondary">
+              MODE
+            </span>
+          </h1>
+          {currentTheme === "dark" ? (
+            <button
+              className="bg-black-700 hover: bg-black w-28 rounded-md border-purple-400 border-2 p-4"
+              onClick={() => {
+                setTheme("light");
+              }}
+            >
+              {""}
+              <Image src="/sun.svg" height="50" width="50" alt="logo" />
+            </button>
+          ) : (
+            <button
+              className="bg-gray-100 w-28 rounded-md border-purple-400 border-2 p-4 hover:bg-gray-400"
+              onClick={() => {
+                setTheme("dark");
+              }}
+            >
+              <Image src="/moon.svg" height="50" width="50" alt="logo" />
+            </button>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  theme: {},
-  variants: {},
+  darkMode: "class",
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      accent: {
+        primary: { DEFAULT: "#202938", dark: "#ffb347" },
+        secondary: { DEFAULT: "#333a4d", dark: "#ffa533" },
+      },
+      background: { DEFAULT: "#f3f3f3", dark: "#1c1c1c" },
+      foreground: { DEFAULT: "#ffffff", dark: "#3a3a3a" },
+      font: {
+        primary: { DEFAULT: "#1c1c1c", dark: "#fdfdfd" },
+        secondary: "#989898",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Lato", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
   plugins: [],
 };
